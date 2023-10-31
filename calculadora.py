a0bccf215d4e1e9934386e2ab8a5be9cdea7b1b9
Franco Gareis <franco.gareis@mirgor.com>
2023-10-31 18:28:39 -0300

    Add: function to save history

--- calculadora.py
+++ calculadora.py
@@ -4,21 +4,38 @@ class Calculadora:
         self.primer_numero = primer_numero
         self.segundo_numero = segundo_numero
         
+    def guardar_historial(self, mensaje):
+        with open("history.txt", 'a') as fh:
+            fh.writelines(mensaje)
+
     def sumar(self):
         """Suma dos numeros y retorna su resultado"""
-        print(f"La suma de {self.primer_numero} y {self.segundo_numero} es igual a: {self.primer_numero + self.segundo_numero}\n")
+        resultado = f"{self.primer_numero + self.segundo_numero}"
+        mensaje = f"La suma de {self.primer_numero} y {self.segundo_numero} es igual a: {resultado}\n"
+        self.guardar_historial(mensaje)
+        print(mensaje)
     
     def restar(self):
         """Resta dos numeros y retorna su resultado"""
-        print(f"La resta de {self.primer_numero} y {self.segundo_numero} es igual a: {self.primer_numero - self.segundo_numero}\n")
+        resultado = f"{self.primer_numero - self.segundo_numero}"
+        mensaje = f"La resta de {self.primer_numero} y {self.segundo_numero} es igual a: {resultado}\n"
+        self.guardar_historial(mensaje)
+        print(mensaje)
     
     def multiplicar(self):
         """Multiplica dos numeros y retorna su resultado"""
-        print(f"La multiplicacion de {self.primer_numero} y {self.segundo_numero} es igual a: {self.primer_numero * self.segundo_numero}\n")
+        resultado = f"{self.primer_numero * self.segundo_numero}"
+        mensaje = f"La multiplicacion de {self.primer_numero} y {self.segundo_numero} es igual a: {resultado}\n"
+        self.guardar_historial(mensaje)
+        print(mensaje)
     
     def dividir(self):
         """Divide dos numeros y retorna su resultado"""
         try:
-            print(f" La division de {self.primer_numero} y {self.segundo_numero} es igual a: {self.primer_numero // self.segundo_numero}\n")
+            resultado = f"{self.primer_numero // self.segundo_numero}"
+            mensaje = f"La division de {self.primer_numero} y {self.segundo_numero} es igual a: {resultado}\n"
+            self.guardar_historial(mensaje)
+            print(mensaje)
         except ZeroDivisionError:
-            print("No se puede dividir un numero zero")
+            print("\nNo se puede dividir un numero zero")
+            
